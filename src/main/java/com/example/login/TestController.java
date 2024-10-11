5c438e0ad0896a4691ac788362631d780eeb829a
Tom Sandt <tomsandt206@gmail.com>
2024-10-11 13:55:12 +0200

    test1

--- src/main/java/com/example/login/TestController.java
+++ src/main/java/com/example/login/TestController.java
@@ -20,7 +20,7 @@ public class TestController {
 		this.userService = userService;
 	}
 	
-	@GetMapping("/TEST2")
+	@GetMapping("/TESTTTT2")
 	public String showLogin(Model model) {
 		model.addAttribute("user", new User());
 		return "index";
