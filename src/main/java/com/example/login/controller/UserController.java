66b5c136b472b0d845fb05570982e3112a2ab3e3
Tom Sandt <tomsandt206@gmail.com>
2024-10-16 11:19:13 +0200

    pwd-hash change

--- src/main/java/com/example/login/controller/UserController.java
+++ src/main/java/com/example/login/controller/UserController.java
@@ -21,7 +21,7 @@ public class UserController {
 	@Autowired
 	private Util util;
 	
-	@GetMapping("/")
+	@GetMapping("/pwd-hash")
 	public String showLogin(Model model) {
 		model.addAttribute("userDTO", new UserDTO());
 		return "index";
